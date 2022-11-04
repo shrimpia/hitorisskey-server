2cd18345fa62595add26779287b412cc76a7a7d4
Xeltica <xeltica@gmail.com>
2022-11-04 18:28:11 +0900

    404

---
+++ packages/frontend/src/pages/[...all].tsx
@@ -0,0 +1,45 @@
+import { Component } from "solid-js";
+import { styled } from "solid-styled-components";
+
+import { useTitle } from "../hooks/use-title";
+import { PostView } from "../components/views/post-view";
+import { Post } from "../api/models/post";
+
+const NotFound: Component = () => {
+  useTitle('Not Found');
+
+  const fakePost: Post = {
+    id: '',
+    channel: '',
+    annotation: null,
+    content: '残念ながら、存在しないページに来てしまったみたいです。\n\nリンク切れや、URLの入力ミスがないか確認してみてください。',
+    isMine: false,
+    reactions: [{
+      emoji: '😭',
+      isMine: false,
+    }, {
+      emoji: '😭',
+      isMine: true,
+    }, {
+      emoji: '😭',
+      isMine: false,
+    }, {
+      emoji: '🤯',
+      isMine: false,
+    }],
+  };
+
+  const Wrapper = styled.div`
+    transform-origin: center center;
+    transform: rotate(2deg);
+    margin-top: 32px;
+  `;
+
+  return (
+    <Wrapper class="shadow-2">
+      <PostView post={fakePost}/>
+    </Wrapper>
+  );
+}
+
+export default NotFound;
